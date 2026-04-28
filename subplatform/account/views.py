36bdf674337bebb6fd7370056d78ee60957537ac
uddinnahim <nahim.211902019@gmail.com>
2026-04-28 23:19:21 +0600

    feat: Initialize projects and Adding crispy bootstrap

---
+++ subplatform/account/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render , redirect
+from .forms import CustomUserCreationForm
+
+from django.contrib.auth.forms import AuthenticationForm
+
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+# Create your views here.
+
+def home(request):
+    return render(request, 'account/index.html')
+
+
+def register(request):
+    form = CustomUserCreationForm()
+    if request.method == "POST":
+
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('my-login')
+            # return HttpResponse('Registration successful! You can now log in.')
+    
+    context = {
+        'RegistrationForm': form,
+    }
+
+    return render(request, 'account/register.html', context)
+
+
+def my_login(request):
+
+    form = AuthenticationForm()
+
+    if request.method == "POST":
+
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request,username=username, password=password)
+            if user is not None and user.is_writer == True: 
+                login(request, user)
+                return HttpResponse('Login successful! Welcome, Writer!')
+
+            if user is not None and user.is_writer == False:
+                login(request, user)
+                return HttpResponse('Login successful! Welcome, Reader!')
+
+    context = {
+            'LoginForm': form,
+        }
+
+        
+    return render(request, 'account/my-login.html', context)
