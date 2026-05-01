c5fcd842ea8ff5da9af4135227f8e5253d01e9ef
uddinnahim <nahim.211902019@gmail.com>
2026-05-02 00:26:33 +0600

    feat: Delete user

--- subplatform/writer/urls.py
+++ subplatform/writer/urls.py
@@ -1,10 +1,11 @@
 from django.urls import  path
-from . import views
+from  . import views
 
 urlpatterns = [
     path('writer-dashboard/', views.writer_dashboard, name='writer-dashboard'),
     path('create-article/', views.create_article, name='create-article'),
     path('my-articles/', views.my_articles, name='my-articles'),
+    path('delete-account/', views.delete_account, name='delete-account'),
     path('update-article/<str:pk>/', views.update_article, name='update-article'),
     path('delete-article/<str:pk>/', views.delete_article, name='delete-article'),
     path('account-management/', views.account_management, name='account-management'),
