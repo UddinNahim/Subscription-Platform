5a13e99b92ab9ffb94e62ad9f68a278329db9ff8
uddinnahim <nahim.211902019@gmail.com>
2026-05-01 20:30:38 +0600

    feat: update article

--- subplatform/writer/models.py
+++ subplatform/writer/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from account.models import CustomUser
+
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField(max_length=10000)
+    date_posted = models.DateTimeField(auto_now_add=True)
+
+    is_premium = models.BooleanField(default=False, verbose_name="Premium Article")
+
+    user = models.ForeignKey(CustomUser, max_length=10 ,  on_delete=models.CASCADE, null=True)
+
--- subplatform/writer/views.py
+++ subplatform/writer/views.py
@@ -1,6 +1,63 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
+
+
+from .forms import ArticleForm
+
+from django.http import HttpResponse
+
+from .models import Article 
 
 @login_required(login_url='my-login')
 def writer_dashboard(request):
     return render(request, 'writer/writer-dashboard.html')
+
+
+@login_required(login_url='my-login')
+def create_article(request):
+    form = ArticleForm()
+
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+
+            article = form.save(commit=False)
+            article.user = request.user
+            article.save()
+            return  redirect('my-articles')
+    
+    context = {'CreateArticleForm': form}
+    return render(request, 'writer/create-article.html', context)
+
+@login_required(login_url='my-login')
+def my_articles(request):
+
+    current_user = request.user.id
+
+    article = Article.objects.all().filter(user=current_user)
+
+    context = {'AllArticles': article}
+
+    return render(request, 'writer/my-articles.html', context)
+
+@login_required(login_url='my-login')
+def update_article(request, pk):
+
+    article = Article.objects.get(id=pk)
+
+    form = ArticleForm(instance=article)
+
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect('my-articles')
+
+    context = {'UpdateArticleForm': form}
+    return render(request, 'writer/update-article.html', context)
+
+
+
+@login_required(login_url='my-login')
+def account_management(request):
+    pass
