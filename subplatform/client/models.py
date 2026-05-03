8777ec540bb4c2dcaa2b920d7135d5ebee951240
uddinnahim <nahim.211902019@gmail.com>
2026-05-04 01:00:25 +0600

    feat: client subscription setup

--- subplatform/client/models.py
+++ subplatform/client/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from account.models import CustomUser
 
 # Create your models here.
+
+class Subscription(models.Model):
+
+    subscriber_name = models.CharField(max_length=255)
+    subscription_plan = models.CharField(max_length=255)
+    subscription_cost = models.CharField(max_length=255)
+
+    paypal_subscription_id = models.CharField(max_length=255, blank=True, null=True)
+
+    is_active = models.BooleanField(default=False)
+
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, unique=True)
+
+
+    def __str__(self):
+        return f'Subscription for {self.subscriber_name} - Plan: {self.subscription_plan} - Cost: {self.subscription_cost} - Active: {self.is_active}'
+        
--- subplatform/client/views.py
+++ subplatform/client/views.py
@@ -1,6 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.contrib.auth.decorators import login_required
+
+from writer.models import Article
+
+from .models import Subscription
+
 
 @login_required(login_url='my-login')
 def client_dashboard(request):
-    return render(request, 'client/client-dashboard.html')
+    try:
+        subDetails = Subscription.objects.get(user=request.user)
+        subscription_plan = subDetails.subscription_plan
+
+        context = {
+            'subscription_plan': subscription_plan,
+        }
+        return render(request, 'client/client-dashboard.html', context)
+    except Subscription.DoesNotExist:
+        subscription_plan = None
+        context = {
+            'subscription_plan': subscription_plan,
+        }
+        return render(request, 'client/client-dashboard.html', context)
+
+
+@login_required(login_url='my-login')
+def browse_articles(request):
+
+    try:
+        subDetails = Subscription.objects.get(user=request.user, is_active=True)
+    except Subscription.DoesNotExist:
+        return render(request, 'client/subscription-locked.html')
+    
+    current_subscription_plan = subDetails.subscription_plan
+    if current_subscription_plan == 'standard':
+        articles = Article.objects.all().filter(is_premium=False)
+    elif current_subscription_plan == 'premium':
+        articles = Article.objects.all()
+
+        # context Django view-এর একটা খুব গুরুত্বপূর্ণ অংশ। সহজভাবে বললে, context হলো data container (ডাটা পাঠানোর মাধ্যম) — যেটা দিয়ে backend (view) থেকে frontend (template)-এ ডাটা পাঠানো হয়।
+
+    context = {
+        'AllClientArticles': articles,
+    }
+
+    return render(request, 'client/browse-articles.html', context)
+
+
+@login_required(login_url='my-login')
+def subscription_locked(request):
+    return render(request, 'client/subscription-locked.html')
